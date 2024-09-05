c7fe4e5ab448f8e98579d55897f3d33ca0fba752
majam <majam@MAJA>
2024-09-05 20:20:35 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BarkBuddy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
